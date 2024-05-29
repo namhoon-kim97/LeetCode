6693249892f29be51e33ad249c42d55672f201cc
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-05-29 15:41:30 +0900

    Time: 8 ms (78.67%), Space: 13.9 MB (40.49%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ret;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    ret.push_back(i);
-                    ret.push_back(j);
-                    return ret;
-                }
-            }
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int comp = target - nums[i];
+            if (map.find(comp) != map.end())
+                return { map[comp], i };
+            map[nums[i]] = i;
         }
-        return {};  
+        return {};
     }
 };
