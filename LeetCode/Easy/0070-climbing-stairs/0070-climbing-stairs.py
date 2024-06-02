86afcd2632daf4cc3a5b17253a38d440b74b4ea3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-06-03 00:22:09 +0900

    Time: 37 ms (38.43%), Space: 16.5 MB (78.48%) - LeetHub

---
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n < 3:
+            return n
+        d = [0] * (n + 1)
+        d[0] = 0
+        d[1] = 1
+        d[2] = 2
+      
+
+        for i in range(3, n + 1):
+            d[i] = d[i - 1] + d[i - 2]
+        return d[n]
