7cdac0fd154ace0f1caf7790ac152a27ddf07e61
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-06-03 16:03:30 +0900

    Time: 74 ms (66.20%), Space: 17.8 MB (79.64%) - LeetHub

--- LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ LeetCode/Medium/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,11 +6,8 @@ class Solution:
             mid = (st + en) // 2
             if nums[mid] < target:
                 st = mid + 1
-            elif nums[mid] > target:
-                en = mid
             else:
                 en = mid
-                flag = True
         return st
 
     def upper(self, nums, target):
@@ -20,29 +17,14 @@ class Solution:
             mid = (st + en) // 2
             if nums[mid] <= target:
                 st = mid + 1
-            elif nums[mid] > target:
-                en = mid
             else:
                 en = mid
-                flag = True
         return st
-
-    def b_search(self, nums, target):
-        st = 0
-        en = len(nums)
-        while st < en:
-            mid = (st + en) // 2
-            if nums[mid] < target:
-                st = mid + 1
-            elif nums[mid] > target:
-                en = mid
-            else:
-                return True
-        return False
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         lower_idx = self.lower(nums, target)
         upper_idx = self.upper(nums, target)
-        if self.b_search(nums, target):
-            return [lower_idx, upper_idx - 1]
-        return [-1, -1]
+
+        if lower_idx == len(nums) or nums[lower_idx] != target:
+            return [-1, -1]
+        return [lower_idx, upper_idx - 1]
