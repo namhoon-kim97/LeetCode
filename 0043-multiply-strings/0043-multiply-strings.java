a2365f86a19509f6c16fe412799b278e75f50394
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-08-18 16:44:51 +0900

    Time: 2 ms (98.95%), Space: 42.3 MB (64.22%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,50 +1,20 @@
 class Solution {
-    String add(String a, String b){
-        StringBuilder sb = new StringBuilder();
-
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-        int carry = 0;
-        
-        while (i >= 0 || j >= 0 || carry > 0){
-            int x = (i >= 0) ? a.charAt(i) - '0' : 0;
-            int y = (j >= 0) ? b.charAt(j) - '0' : 0;
-            int temp = x + y + carry;
-            sb.append(temp % 10);
-            carry = temp / 10;
-            i--; j--;
-        }
-
-        return sb.reverse().toString();
-    }
-
-    String multiple(String s, int time, int idx){
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < idx - 1; i++)
-            sb.append('0');
-        
-        int carry = 0;
-        for (int i = s.length() - 1; i >=  0; i--){
-           int cur = s.charAt(i) - '0';
-           int temp = cur * time + carry;
-           sb.append(temp % 10);
-           carry = temp / 10;
-        }
-        if (carry != 0) sb.append(carry);
-        return sb.reverse().toString();
-    }
-    
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
-        int n1 = num1.length();
-        int n2 = num2.length();
-        String res = "";
-        for (int i = n1 - 1; i >= 0; i--){
-            int digit = num1.charAt(i) - '0';
-            if (digit == 0) continue;
-            String part = multiple(num2, digit, n1 - i);
-            res = res.isEmpty() ? part : add(res, part);
+       int n = num1.length(), m = num2.length();
+       int[] prod = new int[n + m];
+       for (int i = n - 1; i  >= 0; i--){
+        int x = num1.charAt(i) - '0';
+        for (int j = m - 1; j >= 0; j--){
+            int y = num2.charAt(j) - '0';
+            int temp = x * y + prod[i+j+1];
+            prod[i+j+1] = temp % 10;
+            prod[i+j] += temp / 10;
         }
-        return res.isEmpty() ? "0" : res;
+       }
+       StringBuilder sb = new StringBuilder();
+       int k = 0;
+       while(k < prod.length && prod[k] == 0) k++;
+       for (; k < prod.length; k++) sb.append(prod[k]);
+       return sb.length() == 0 ? "0" : sb.toString();
     }
 }
