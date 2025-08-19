c44b6c6caf02ecdd7d07f46887a56893d6ce6c2e
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-08-19 14:50:42 +0900

    Time: 17 ms (20.12%), Space: 45.1 MB (73.23%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int jump(int[] nums) {
+        if (nums.length == 1) return 0;
+        int cnt = 0;
+        for (int i = nums.length - 1; i > 0; ){
+            boolean flag = false;
+            int temp = 0;
+            for (int j = i - 1; j >= 0; j--){
+                if (i - j <= nums[j]){
+                    temp = j;
+                    flag = true;
+                }
+            }
+            if(flag) {
+                cnt++;
+                i = temp;
+            }
+            else i--;
+        }
+        return cnt;
+    }
+}
