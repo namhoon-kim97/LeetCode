cd62a3f73854f5d31187671477cb6e1689fb4741
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-08-25 15:19:49 +0900

    Time: 1 ms (71.82%), Space: 45.7 MB (67.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,49 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        boolean firstRow = false;
-        boolean firstCol = false;
-        for (int i = 0; i < n; i++)
-            if (matrix[i][0] == 0) {
-                firstRow = true;
-                break;
-            }
-        
-        for (int i = 0; i < m; i++){
-            if (matrix[0][i] == 0){
-                firstCol = true;
-                break;
-            }
-        }
-        for (int i = 1; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-        for (int i = 1; i < n; i++){
-            if (matrix[i][0] == 0){
-                for (int j = 1; j < m; j++){
-                    matrix[i][j] = 0;
+        int n = matrix.length, m = matrix[0].length;
+
+        int col0 = 1; // 첫 번째 열에 0이 있었는지 표시(0이면 첫 열을 나중에 0으로)
+        // 1) 마킹: 첫 행은 matrix[0][j], 첫 열은 matrix[i][0]
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0; // 첫 열 플래그
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // 행 마크
+                    matrix[0][j] = 0; // 열 마크
                 }
             }
         }
 
-        for (int i = 1; i < m; i++){
-            if (matrix[0][i] == 0){
-                for (int j = 1; j < n; j++){
-                    matrix[j][i] = 0;
+        // 2) 뒤에서 앞으로 채우기: 마커에 따라 0 세팅
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
+            // 첫 번째 열 처리(마지막에)
+            if (col0 == 0) matrix[i][0] = 0;
         }
-        if (firstRow)
-            for (int i = 0; i < n; i++)
-                matrix[i][0] = 0;
-        if (firstCol)
-            for (int i = 0; i < m; i++)
-                matrix[0][i] = 0;
     }
 }
