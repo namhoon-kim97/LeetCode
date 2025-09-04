27c1a7b5782101b2045e008a6c513ff713e1739e
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-09-04 17:45:32 +0900

    Time: 6 ms (48.71%), Space: 45.9 MB (82.91%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,13 +1,25 @@
 class Solution {
+    boolean bsearch(int target, int[] matrix){
+        int n = matrix.length;
+        int st = 0;
+        int en = n - 1;
+        while (st <= en){
+            int mid = (st + en) / 2;
+            if (matrix[mid] < target) st = mid + 1;
+            else if (matrix[mid] == target) return true;
+            else en = mid - 1;
+        }
+        return false;
+    }
+  
     public boolean searchMatrix(int[][] matrix, int target) {
-        Set<Integer> ret = new HashSet<>();
         int n = matrix.length;
         int m = matrix[0].length;
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                ret.add(matrix[i][j]);
+            if (matrix[i][0] <= target && target <= matrix[i][m-1]){
+                if (bsearch(target, matrix[i])) return true;
             }
         }
-        return ret.contains(target);
+        return false;
     }
 }
