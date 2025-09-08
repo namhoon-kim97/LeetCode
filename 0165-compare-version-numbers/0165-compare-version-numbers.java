5c26830b0abdcc331c6740fb58376b292e376feb
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-09-08 14:26:19 +0900

    Time: 0 ms (100%), Space: 41.4 MB (93.24%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,34 +1,26 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
+        int n1 = version1.length();
+        int n2 = version2.length();
         int i = 0;
         int j = 0;
-        while (i < v1.length && j < v2.length){
-            int first = Integer.parseInt(v1[i]);
-            int second = Integer.parseInt(v2[j]);
-            System.out.println(first + " "  + second);
-            if (first < second){
-                return -1;
-            } else if (first > second){
-                return 1;
+
+        while (i < n1 || j < n2){
+            int a = 0;
+            int b = 0;
+            while (i < n1 && version1.charAt(i) != '.'){
+                a = a * 10 + (version1.charAt(i) - '0');
+                i++;
             }
-            i++;
-            j++;
+            while (j < n2 && version2.charAt(j) != '.'){
+                b = b * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            if (a < b )return -1;
+            if (a > b )return 1;
+            if (i < n1 && version1.charAt(i) == '.') i++;
+            if (j < n2 && version2.charAt(j) == '.') j++;
         }
-        if (i != v1.length) {
-            for (int k = i; k < v1.length; k++)
-                if (Integer.parseInt(v1[k]) != 0)
-                    return 1;
-            return 0;
-        }
-        if (j != v2.length) {
-            for (int k = j; k < v2.length; k++)
-                if (Integer.parseInt(v2[k]) != 0)
-                    return -1;
-            return 0;
-        }
-        
         return 0;
     }
 }
