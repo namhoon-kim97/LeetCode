7e4cbcb604041e6c2e4720d951fe5d6609a69ee3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-09-11 14:33:10 +0900

    Time: 45 ms (66.41%), Space: 111.3 MB (94.06%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,57 +1,22 @@
-class LRUCache {
-    class Node {
-        int k,v;
-        Node prev, next;
-        Node(int k, int v) {this.k = k; this.v = v;}
-    }
-    Map<Integer, Node> m = new HashMap<>();
-    int capacity = 0;
-    Node head = new Node(0,0);
-    Node tail = new Node(0,0);
+class LRUCache extends LinkedHashMap<Integer, Integer>{
+    private int capacity;
 
     public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
         this.capacity = capacity;
-        head.next = tail; tail.prev = head;
     }
     
     public int get(int key) {
-        Node n = m.get(key);
-        if (n == null) return -1;
-        moveToFront(n);
-        return n.v;
+        return super.getOrDefault(key, -1);
     }
     
     public void put(int key, int value) {
-        if (capacity == 0) return;
-        Node n = m.get(key);
-        if (n != null){
-            n.v = value;
-            moveToFront(n);
-            return;
-        }
-        if (m.size() == capacity){
-            Node lru = tail.prev;
-            remove(lru);
-            m.remove(lru.k);
-        }
-        Node nn = new Node(key, value);
-        addFirst(nn);
-        m.put(key, nn);
+        super.put(key, value);
     }
 
-    void moveToFront(Node n){
-        remove(n);
-        addFirst(n);
-    }
-    void remove(Node n){
-        n.prev.next = n.next;
-        n.next.prev = n.prev;
-    }
-    void addFirst(Node n){
-        n.next = head.next;
-        head.next.prev = n;
-        n.prev = head;
-        head.next = n;
+    @Override
+    public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
+        return size() > capacity;
     }
 }
 
