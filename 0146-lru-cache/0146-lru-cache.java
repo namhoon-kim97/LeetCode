4eabf1b7abb04c46bbdf85d05a923e80bd2164c3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-09-23 19:48:29 +0900

    Time: 44 ms (74.58%), Space: 112.1 MB (85.31%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,22 +1,24 @@
-class LRUCache extends LinkedHashMap<Integer, Integer>{
-    private int capacity;
+class LRUCache {
+    LinkedHashMap<Integer, Integer> lm;
+    int capacity = 0;
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75f, true);
+        lm = new LinkedHashMap<>(capacity, 0.75f, true);
         this.capacity = capacity;
     }
     
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        return lm.getOrDefault(key, -1);
     }
     
     public void put(int key, int value) {
-        super.put(key, value);
-    }
-
-    @Override
-    public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest){
-        return size() > capacity;
+        lm.put(key, value);
+        if (lm.size() > capacity){
+            for (Map.Entry<Integer, Integer> m : lm.entrySet()){
+                lm.remove(m.getKey());
+                break;
+            }
+        }
     }
 }
 
