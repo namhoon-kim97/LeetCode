d1bde61b76fc931437a8431bb1573ea4d16f4883
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-12-19 15:42:30 +0900

    Time: 0 ms (100%), Space: 20.5 MB (61.65%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int d[10001];
     int jump(vector<int>& nums) {
         int n = nums.size();
-        fill(d, d + n, INT_MAX);
-        if (n < 2) return 0;
-        d[0] = 0;
-        for (int i = 1; i < n; i++){
-            for (int j = 0; j < i; j++){
-                if (nums[j] >= i - j)
-                    d[i] = min(d[i], d[j] + 1);
+        int far = nums[0];
+        int cur = 0;
+        int jump = 0;
+        for (int i = 0; i < n - 1; i++){
+            far = max(far, i + nums[i]);
+            if (i == cur){
+                jump++;
+                cur = far;
             }
         }
-        // for (int i = 0; i < n; i++)
-        //     cout << d[i] << ' ';
-        return d[n-1];
+        return jump;
     }
 };
