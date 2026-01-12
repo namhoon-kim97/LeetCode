42efaa4f9e0f9607af26a2dba29dc654c2d9e15c
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2026-01-12 22:22:29 +0900

    Time: 0 ms (100%), Space: 10.3 MB (70.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,12 +5,10 @@ public:
         if (nums.size() == 0 ) return 0;
         dp[0] = nums[0];
         if (nums.size() == 1) return nums[0];
-        dp[1] = nums[1];
-        if (nums.size() == 2) return max(dp[0], dp[1]);
+        dp[1] = max(nums[0],nums[1]);
+        if (nums.size() == 2) return dp[1];
         for (int i = 2; i < nums.size(); i++){
             dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
-            for (int j = 0; j < i - 2; j++)
-                dp[i] = max(dp[i], dp[j] + nums[i]);
         }
         return dp[nums.size() - 1];
     }
