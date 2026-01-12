df234403964d0ff953fdd6c7fc3c65d26524eb1c
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2026-01-12 21:32:23 +0900

    Time: 0 ms (100%), Space: 8.6 MB (33.59%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n < 3) return n;
+        int* d = new int[n+1];
+        // d[i] = d[i-1] + d[i-2]
+        d[0] = 0;
+        d[1] = 1;
+        d[2] = 2;
+       
+        for (int i = 3; i <= n; i++){
+            d[i] = d[i-1] + d[i-2];
+        }
+        return d[n];
+    }
+};
