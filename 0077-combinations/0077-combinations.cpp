df0cf6ad2c06c557abde56fd165080a0d3409451
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2026-01-19 21:53:58 +0900

    Time: 290 ms (6.54%), Space: 159.7 MB (6.89%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> ret;
-    set<int> vis;
-    void comb(int idx, int cnt, vector<int> temp, int n){
-        if (cnt == 0) {
+    int N,K;
+    void comb(int idx, vector<int> temp){
+        if (temp.size() == K) {
             ret.push_back(temp);
             return;
         }
-        for (int i = idx; i <= n; i++){
-            if (vis.find(i) != vis.end()) continue;
-            vis.insert(i);
+        for (int i = idx; i <= N; i++){
             temp.push_back(i);
-            comb(i, cnt - 1, temp,n);
+            comb(i + 1, temp);
             temp.pop_back();
-            vis.erase(i);
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        comb(1,k, {}, n);
+        N = n; K = k;
+        comb(1, {});
         return ret;
     }
 };
