bdd0dee55cc56a3a2a8024e185e1cf0a6ee97a78
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2026-01-28 00:12:11 +0900

    Time: 0 ms (100%), Space: 14.9 MB (19.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,8 +4,7 @@ public:
         unordered_map<int,int> um; // value -> idx
         for (int i = 0; i < nums.size(); i++){
             int need = target - nums[i];
-            auto it = um.find(need);
-            if (it != um.end()) return {it->second, i};
+            if (um.count(need)) return {um[need], i};
             um[nums[i]] = i;
         }
         return {};
