c25cc10722e91e078820cd772085d661be8e74d7
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2026-04-10 21:38:17 +0900

    Time: 38 ms (46.47%), Space: 128.5 MB (19.14%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -1,52 +1,47 @@
 class Robot {
 public:
-    int dx[4] = {1, 0, -1, 0};
-    int dy[4] = {0, 1, 0, -1};
-    enum class Direction { East = 0, North = 1, West = 2, South = 3 };
-
-    Robot(int width, int height)
-        : _x(0), _y(0), _w(width), _h(height), _dir(Direction::East) {}
+    Robot(int width, int height) {
+        w = width;
+        h = height;
+        perimeter = 2 * (w - 1) + 2 * (h - 1);
+        pos = 0;
+        moved = false;
+    }
 
     void step(int num) {
-        int perimeter = 2 * (_w - 1) + 2 * (_h - 1);
-        num %= perimeter;
-        if (num == 0) {
-            if (_x == 0 && _y == 0) {
-                _dir = Direction::South;
-            }
-        }
-        while (num) {
-            int nx = _x + dx[static_cast<int>(_dir)];
-            int ny = _y + dy[static_cast<int>(_dir)];
-            if (nx < 0 || ny < 0 || nx >= _w || ny >= _h) {
-                _dir = static_cast<Direction>((static_cast<int>(_dir) + 1) % 4);
-                continue;
-            }
-            _x = nx;
-            _y = ny;
-            num--;
+        moved = true;
+        pos = (pos + num) % perimeter;
+    }
+
+    vector<int> getPos() {
+        if (pos < w) {
+            return {pos, 0}; // Bottom edge
+        } else if (pos < w + h - 1) {
+            return {w - 1, pos - w + 1}; // Right edge
+        } else if (pos < 2 * w + h - 2) {
+            return {2 * w + h - 3 - pos, h - 1}; // Top edge
+        } else {
+            return {0, perimeter - pos}; // Left edge
         }
     }
 
-    vector<int> getPos() { return {_x, _y}; }
-
     string getDir() {
-        string names[] = {"East", "North", "West", "South"};
-        return names[static_cast<int>(_dir)];
+        if (pos == 0)
+            return moved ? "South" : "East";
+
+        if (pos < w)
+            return "East";
+        if (pos < w + h - 1)
+            return "North";
+        if (pos < 2 * w + h - 2)
+            return "West";
+
+        return "South";
     }
 
 private:
-    int _x;
-    int _y;
-    int _w;
-    int _h;
-    Direction _dir;
+    int w, h;
+    int perimeter;
+    int pos;
+    bool moved;
 };
-
-/**
- * Your Robot object will be instantiated and called as such:
- * Robot* obj = new Robot(width, height);
- * obj->step(num);
- * vector<int> param_2 = obj->getPos();
- * string param_3 = obj->getDir();
- */
